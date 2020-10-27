8ec3fd3d03c871838ff6ae0a05d151e82483eff2
ahmed <ahmed.elghoul@infor.com>
2020-10-27 21:47:58 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookmarksPageComponent } from './bookmarks-page/bookmarks-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'bookmarks', component: BookmarksPageComponent },
+  { path: 'details', loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BookmarksPageComponent } from './bookmarks-page/bookmarks-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    BookmarksPageComponent
   ],
   imports: [
     BrowserModule,
